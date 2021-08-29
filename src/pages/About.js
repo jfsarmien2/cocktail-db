d93da89f896f5381d71f66936feb8223f54e490e
John S <john.sarmiento452@gmail.com>
2021-08-29 18:22:19 +0800

    finished project

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>Poutine readymade synth, leggings quinoa banh mi mlkshk. Gastropub disrupt wayfarers, brunch offal venmo kombucha direct trade adaptogen pickled. Succulents polaroid bicycle rights umami iceland ugh, williamsburg tote bag lo-fi cred coloring book vegan pop-up letterpress deep v. Narwhal 8-bit leggings four dollar toast. Tacos keytar gluten-free, vaporware bicycle rights quinoa tumblr flexitarian biodiesel 3 wolf moon letterpress green juice PBR&B. Bicycle rights shoreditch fingerstache synth plaid drinking vinegar green juice neutra vaporware enamel pin farm-to-table ennui.</p>
+    </section>
+  )
+}
+
+export default About
